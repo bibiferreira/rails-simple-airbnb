159f665a9cae4c91ccbb1a529356f48b9e55208d
Bibi Ferreira <bibi.espeiorin@gmail.com>
2022-02-16 21:10:15 +0100

    initial flat model and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Apartamento do Gido',
+  address: 'Bento Gonçalves',
+  description: 'Ap com vista pra cidade, 1 suite',
+  price_per_night: 81,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Rosseli Suite',
+  address: 'Centro Storico, Firenze',
+  description: 'A lovely balcony. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 89,
+  number_of_guests: 5
+)
+
+
+Flat.create!(
+  name: 'Sitio do Gilson',
+  address: 'Garuva',
+  description: 'Galos e galinhas everywhere',
+  price_per_night: 50,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Cobertura da Soninha',
+  address: 'Rua Tangara',
+  description: 'Terreno, churrasqueira, piscina e cachorra chata',
+  price_per_night: 99,
+  number_of_guests: 5
+)
